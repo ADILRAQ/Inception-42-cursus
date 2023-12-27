9969cc4c1a9206f20a3681668f8c3ea655ae11ed
adilraq <raqiouiadil852@gmail.com>
2023-12-27 18:23:30 +0100

    MOD: Fuck it

---
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -0,0 +1,27 @@
+# !/bin/bash
+
+service mariadb start
+
+# mysqld_safe &
+
+mysqld_safe --skip-grant-tables &
+
+# Create a user
+mysql -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}@localhost IDENTIFIED BY '${MARIADB_PASSWORD}';"
+
+# Give it permission on all databases
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${MARIADB_USER}@localhost;"
+
+# Creata a database
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
+
+# Add a password to the root user
+mysql -e "ALTER USER root@localhost IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+
+# Reload privileges
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p${MARIADB_ROOT_PASSWORD} shutdown
+
+#exec mysqld_safe
+bash 
