fbd3d69071238a1f0d5e7a152fd761d6ccb72d37
ADILRAQ <raqiouiadil852@gmail.com>
2023-12-29 14:59:29 -0500

    MOD: Near to get done

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -5,24 +5,22 @@ service mariadb start
 sleep 2
 
 # Create a user
-mysql -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@localhost IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
 
 # Give it permission on all databases
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@localhost;"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%';"
 
 # Creata a database
 mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
 
 # Add a password to the root user
-# mysql -e "ALTER USER root@localhost IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mysql -e "ALTER USER root@localhost IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
 
 # Reload privileges
-mysql -u"${MARIADB_USER}"  -p"${MARIADB_PASSWORD}" -e "FLUSH PRIVILEGES;"
-# mysql -u"root"  -p"${MARIADB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+# mysql -u"${MARIADB_USER}"  -p"${MARIADB_PASSWORD}" -e "FLUSH PRIVILEGES;"
+mysql -u root  -p"${MARIADB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u"${MARIADB_USER}" -p${MARIADB_PASSWORD} shutdown
+# mysqladmin -u"${MARIADB_USER}" -p"${MARIADB_PASSWORD}" shutdown
+mysqladmin -u root -p"${MARIADB_ROOT_PASSWORD}" shutdown
 
-# exec mysqld_safe
-mysqld
-# mysqld --user="root"
-# bash
+mysqld_safe
--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,13 +1,20 @@
 # !/bin/bash
 
-# sleep 10
+sleep 10
 
 # wp config create --allow-root --dbname=${MARIADB_DATABASE} --dbuser=${MARIADB_USER} --dbpass=${MARIADB_PASSWORD} --dbhost=mariadb:3306 --path='/var/www/wordpress'
 wp config set --allow-root DB_NAME ${MARIADB_DATABASE}
 wp config set --allow-root DB_USER ${MARIADB_USER}
 wp config set --allow-root DB_PASSWORD ${MARIADB_PASSWORD}
-wp config set --allow-root DB_HOST mariadb:/run/mysqld/mysqld.sock
+wp config set --allow-root DB_HOST mariadb:3306
 
-# /usr/sbin/php-fpm7.4 -F
+# TODO: Handle this
+wp core install --allow-root --url="localhost" --title="This a website" --admin_user="sup" --admin_password="hello123" --admin_email="araqioui@student.1337.ma"
 
-bash
+mkdir /run/php
+
+echo "SERVER IS RUNNING......"
+
+/usr/sbin/php-fpm7.4 -F
+
+# bash
